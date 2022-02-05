2b491e8a6a1abc1fd54791e59056e8b7ed2e7efb
bazinga <1065564624@qq.com>
2022-02-05 11:27:10 +0800

    initialize

---
+++ src/main/java/com/bazinga/springboot_web_learning/SpringbootWebLearningApplication.java
@@ -0,0 +1,13 @@
+package com.bazinga.springboot_web_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bazinga/springboot_web_learning/SpringbootWebLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazinga.springboot_web_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
